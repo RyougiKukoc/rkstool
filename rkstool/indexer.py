abf923ba6f8bccc4b6c95d65a4b905265aa1e9f9
RyougiKukoc <st_stars@163.com>
2023-07-05 12:20:25 +0800

    add decode, fully reprod ks2index

--- rkstool/indexer.py
+++ rkstool/indexer.py
@@ -1,16 +1,20 @@
 import os
 import datetime
 import vapoursynth as vs
+import subprocess as sp
 from .mpls_chapter import read_mpls
 from .logger import get_logger
 
 
-def index(workspace_fp: str, logger_fp: str = None):
+def index(workspace_fp: str, logger_fp: str = None, ffprobe_fp: str = None):
     workspace_fp = os.path.abspath(workspace_fp)
     if logger_fp is None:
         logger_fn = datetime.datetime.now().strftime(r'%Y%m%d-%H%M%S') + '.indexer.log'
         logger_fp = os.path.join(workspace_fp, logger_fn)
     logger = get_logger(logger_fp)
+    if ffprobe_fp is None:  # Already added ffprobe in system PATH
+        ffprobe_fp = 'ffprobe'
+    ffcmd = f'"{ffprobe_fp}" -v error -threads auto -show_frames -select_streams v:0 -i'
 
     for bd in os.listdir(workspace_fp):
         tar_fp = os.path.join(workspace_fp, bd)
@@ -20,10 +24,26 @@ def index(workspace_fp: str, logger_fp: str = None):
             stream_fp = os.path.join(tar_fp, 'BDMV', 'STREAM')
             for m2ts in os.listdir(stream_fp):
                 if m2ts.endswith('.m2ts'):
+                    logger.info(f'Indexing {bd}: {m2ts}...')
+                    nvs = False  # No video stream
+                    mfp = os.path.join(stream_fp, m2ts)
                     try:
-                        logger.info(f'Indexing {bd}: {m2ts}')
-                        vs.core.lsmas.LWLibavSource(os.path.join(stream_fp, m2ts))
+                        vs.core.lsmas.LWLibavSource(mfp)
                     except vs.Error:
-                        logger.info(f'{bd}: {m2ts} has no video stream!')
-                        with open(os.path.join(stream_fp, m2ts + '.nvd'), 'w') as nvd:
-                            nvd.write('No video stream.')
+                        nvs = True
+                        with open(os.path.join(stream_fp, m2ts + '.vserr'), 'w') as nvsf:
+                            nvsf.write('No video stream.')
+                    if nvs:
+                        logger.info(f'No video stream in {bd}: {m2ts}, skip.')
+                    else:
+                        logger.info('Done, now decoding...')
+                        p = sp.Popen(f'{ffcmd} "{mfp}"', stdout=sp.DEVNULL, stderr=sp.PIPE)
+                        err = p.communicate()[1].decode()
+                        if len(err) > 0:
+                            with open(os.path.join(stream_fp, m2ts + '.vserr'), 'w') as nvsf:
+                                nvsf.write('Decoding error.')
+                            logger.info('Error occurs when decoding:')
+                            logger.info(err)
+                        else:
+                            logger.info(f'Decode {bd}: {m2ts} successfully.')
+                            

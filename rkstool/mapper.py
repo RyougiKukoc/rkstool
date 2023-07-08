d55676bc2dd6a155c64dcf00e5d21c3bc493653f
RyougiKukoc <st_stars@163.com>
2023-07-08 13:45:58 +0800

    safety overwrite

--- rkstool/mapper.py
+++ rkstool/mapper.py
@@ -22,8 +22,11 @@ def mapconfig(
         target_fn = os.path.basename(target_fp)
         name = os.path.splitext(target_fn)[0]
         script_fn = os.path.join(dir_fp, name + config_ext)
-        if os.path.exists(script_fn) and not replace:
-            continue
+        if os.path.exists(script_fn):
+            if not replace:
+                continue
+            else:
+                os.remove(script_fn)  # for safety
         with open(script_fn, 'w') as sf:
             for line in lines:
                 sf.write(line.replace('$src', target_fn).replace('$bas', name))

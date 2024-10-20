bbcea15e11b34bea66f9d41aeae6e11e44e61d31
RyougiKukoc <st_stars@163.com>
2024-10-20 13:12:49 +0800

    Address https://github.com/RyougiKukoc/rkstool/issues/12

--- rkstool/qpfile_maker.py
+++ rkstool/qpfile_maker.py
@@ -8,25 +8,24 @@ def make_qpfile(
     force: bool = False,  # whether to cover existed '*.qpfile'
 ):
     dir = os.path.abspath(dir)
-    for fn in os.listdir(dir):
-        fp = os.path.join(dir, fn)
-        if os.path.isdir(fp):
-            make_qpfile(fp, qp_ext, force)
+    for dirpath, dirnames, filenames in os.walk(dir):
+        if os.path.basename(dirpath) == 'waste_qp':
             continue
-        name, ext = os.path.splitext(fn)
-        if ext not in ['.m2ts']:
-            continue
-        waste_dir = os.path.join(dir, 'waste_qp')
-        tar_qp_fp = os.path.join(dir, name + qp_ext)
-        res_qp_fp = os.path.join(dir, name + '.qpfile')
-        if os.path.exists(res_qp_fp) and not force:
-            continue
-        if os.path.exists(tar_qp_fp):
-            if tar_qp_fp != res_qp_fp:
-                if os.path.exists(res_qp_fp):
-                    os.makedirs(waste_dir, exist_ok=True)
-                    shutil.move(res_qp_fp, waste_dir)
-                os.rename(tar_qp_fp, res_qp_fp)
-        else:
-            with open(res_qp_fp, 'w') as qpf:
-                qpf.write('0 K')
+        for fn in filenames:
+            name, ext = os.path.splitext(fn)
+            if ext not in ['.m2ts']:
+                continue
+            waste_dir = os.path.join(dirpath, 'waste_qp')
+            tar_qp_fp = os.path.join(dirpath, name + qp_ext)
+            res_qp_fp = os.path.join(dirpath, name + '.qpfile')
+            if os.path.exists(res_qp_fp) and not force:
+                continue
+            if os.path.exists(tar_qp_fp):
+                if tar_qp_fp != res_qp_fp:
+                    if os.path.exists(res_qp_fp):
+                        os.makedirs(waste_dir, exist_ok=True)
+                        shutil.move(res_qp_fp, waste_dir)
+                    os.rename(tar_qp_fp, res_qp_fp)
+            else:
+                with open(res_qp_fp, 'w') as qpf:
+                    qpf.write('0 K')

5f474598926df4d495c12e29aff7a9b3147f617d
RyougiKukoc <st_stars@163.com>
2024-10-20 13:57:48 +0800

    Address https://github.com/RyougiKukoc/rkstool/issues/13

--- rkstool/mapper.py
+++ rkstool/mapper.py
@@ -4,7 +4,8 @@ import glob
 
 def map_config(
     config_fp: str, 
-    config_ext = '.vpy', 
+    config_ext = '.vpy',
+    target_fp: str | list = None, 
     replace: bool = False, 
     accept_ext = ['.m2ts'], 
 ):
@@ -12,12 +13,18 @@ def map_config(
     assert os.path.isfile(config_fp)
     with open(config_fp, 'r') as conf:
         lines = conf.readlines()
-
     dir_fp = os.path.dirname(config_fp)
+    if target_fp is None:
+        target_fp = [dir_fp]
+    elif isinstance(target_fp, str):
+        target_fp = [os.path.abspath(target_fp)]
+    else:
+        assert hasattr(target_fp, '__iter__')
+        target_fp = [os.path.abspath(d) for d in target_fp]
     target_list = []
-    for ext in accept_ext:
-        target_list += glob.glob(os.path.join(glob.escape(dir_fp), '*' + ext))
-    
+    for d in target_fp:
+        for ext in accept_ext:
+            target_list += glob.glob(os.path.join(glob.escape(d), '*' + ext))
     for target_fp in target_list:
         target_fn = os.path.basename(target_fp)
         name = os.path.splitext(target_fn)[0]
